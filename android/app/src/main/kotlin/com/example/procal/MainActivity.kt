c80cd72e75f0ab6f0cb8e8e5d7529b92cf3000e3
Kyler Davis <kyler.davis@instride.health>
2024-10-19 18:40:29 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/procal/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.procal
+
+// import io.flutter.embedding.android.FlutterActivity
+// changed to below for android health access
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+// updated from FlutterActivity() for android health access
+class MainActivity: FlutterFragmentActivity()
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Procal'),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+            ],
+          ),
+        ),
+      );
+}
