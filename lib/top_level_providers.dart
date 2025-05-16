66d539a60d67cc758a28948917cddc03988203b3
Kyler Davis <ky.s.dav@gmail.com>
2025-05-16 13:17:50 -0600

    updating

--- lib/top_level_providers.dart
+++ lib/top_level_providers.dart
@@ -12,41 +12,38 @@ final proteinGoalProvider = StateProvider<int?>((_) => null);
 final caloriesConsumedProvider = StateProvider<int?>((_) => null);
 final caloriesGoalProvider = StateProvider<int?>((_) => null);
 
-final healthServiceProvider = Provider<HealthService>(
-    (ref) => HealthService(healthManager: Health(), ref: ref));
+final healthServiceProvider = Provider<HealthService>((ref) => HealthService(healthManager: Health(), ref: ref));
 
-final localStorageServiceProvider =
-    Provider<LocalStorageService>((_) => LocalStorageService());
+final localStorageServiceProvider = Provider<LocalStorageService>((_) => LocalStorageService());
 
-final firebaseAuthProvider =
-    Provider<FirebaseAuth>((_) => FirebaseAuth.instance);
+final firebaseAuthProvider = Provider<FirebaseAuth>((_) => FirebaseAuth.instance);
 
 final currentUserProvider = StreamProvider<User?>((ref) {
   final auth = ref.watch(firebaseAuthProvider);
   return auth.authStateChanges();
 });
 
-final procalRouterProvider =
-    Provider((ref) => _routeConfig(redirect: (context, state) {
-          final authState = ref.watch(currentUserProvider);
-          if (authState.isLoading || authState.hasError) return null;
+final procalRouterProvider = Provider(
+  (ref) => _routeConfig(
+    redirect: (context, state) {
+      final authState = ref.watch(currentUserProvider);
+      if (authState.isLoading || authState.hasError) return null;
 
-          final isAuthenticated = authState.valueOrNull != null;
-          final isAuthenticating = state.matchedLocation == Routes.login.path;
+      final isAuthenticated = authState.valueOrNull != null;
+      final isAuthenticating = state.matchedLocation == Routes.login.path;
 
-          if (!isAuthenticated) {
-            return Routes.login.path;
-          }
+      if (!isAuthenticated) {
+        return Routes.login.path;
+      }
 
-          if (isAuthenticating) {
-            return Routes.home.path;
-          }
+      if (isAuthenticating) {
+        return Routes.home.path;
+      }
 
-          return null;
-        }));
+      return null;
+    },
+  ),
+);
 
 GoRouter _routeConfig({GoRouterRedirect? redirect}) =>
-    GoRouter(redirect: redirect, routes: [
-      Routes.home,
-      Routes.intro,
-    ]);
+    GoRouter(redirect: redirect, routes: [Routes.home, Routes.intro, Routes.login]);
