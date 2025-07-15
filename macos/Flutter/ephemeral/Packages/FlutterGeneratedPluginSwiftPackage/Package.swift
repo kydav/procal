492cd3ac700b2710af7a32272a3b99a302b3b1cb
Kyler Davis <kyler.davis@instride.health>
2025-07-15 17:57:20 -0600

    update

---
+++ macos/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+//  Generated file. Do not edit.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "FlutterGeneratedPluginSwiftPackage",
+    platforms: [
+        .macOS("10.14")
+    ],
+    products: [
+        .library(name: "FlutterGeneratedPluginSwiftPackage", type: .static, targets: ["FlutterGeneratedPluginSwiftPackage"])
+    ],
+    dependencies: [
+        .package(name: "device_info_plus", path: "/Users/kylerdavis/.pub-cache/hosted/pub.dev/device_info_plus-11.5.0/macos/device_info_plus"),
+        .package(name: "firebase_app_check", path: "/Users/kylerdavis/.pub-cache/hosted/pub.dev/firebase_app_check-0.3.2+9/macos/firebase_app_check"),
+        .package(name: "firebase_auth", path: "/Users/kylerdavis/.pub-cache/hosted/pub.dev/firebase_auth-5.6.2/macos/firebase_auth"),
+        .package(name: "firebase_core", path: "/Users/kylerdavis/.pub-cache/hosted/pub.dev/firebase_core-3.15.1/macos/firebase_core"),
+        .package(name: "shared_preferences_foundation", path: "/Users/kylerdavis/.pub-cache/hosted/pub.dev/shared_preferences_foundation-2.5.4/darwin/shared_preferences_foundation")
+    ],
+    targets: [
+        .target(
+            name: "FlutterGeneratedPluginSwiftPackage",
+            dependencies: [
+                .product(name: "device-info-plus", package: "device_info_plus"),
+                .product(name: "firebase-app-check", package: "firebase_app_check"),
+                .product(name: "firebase-auth", package: "firebase_auth"),
+                .product(name: "firebase-core", package: "firebase_core"),
+                .product(name: "shared-preferences-foundation", package: "shared_preferences_foundation")
+            ]
+        )
+    ]
+)
