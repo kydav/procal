2ebf4b0c3ce22972ef6354850c3551c9d3394310
Kyler Davis <kyler.davis@instride.health>
2025-09-21 13:47:38 -0600

    update packages

--- macos/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
+++ macos/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
@@ -15,10 +15,10 @@ let package = Package(
         .library(name: "FlutterGeneratedPluginSwiftPackage", type: .static, targets: ["FlutterGeneratedPluginSwiftPackage"])
     ],
     dependencies: [
-        .package(name: "device_info_plus", path: "/Users/kylerdavis/.pub-cache/hosted/pub.dev/device_info_plus-11.5.0/macos/device_info_plus"),
-        .package(name: "firebase_app_check", path: "/Users/kylerdavis/.pub-cache/hosted/pub.dev/firebase_app_check-0.4.0/macos/firebase_app_check"),
-        .package(name: "firebase_auth", path: "/Users/kylerdavis/.pub-cache/hosted/pub.dev/firebase_auth-6.0.1/macos/firebase_auth"),
-        .package(name: "firebase_core", path: "/Users/kylerdavis/.pub-cache/hosted/pub.dev/firebase_core-4.0.0/macos/firebase_core"),
+        .package(name: "device_info_plus", path: "/Users/kylerdavis/.pub-cache/hosted/pub.dev/device_info_plus-12.1.0/macos/device_info_plus"),
+        .package(name: "firebase_app_check", path: "/Users/kylerdavis/.pub-cache/hosted/pub.dev/firebase_app_check-0.4.0+1/macos/firebase_app_check"),
+        .package(name: "firebase_auth", path: "/Users/kylerdavis/.pub-cache/hosted/pub.dev/firebase_auth-6.0.2/macos/firebase_auth"),
+        .package(name: "firebase_core", path: "/Users/kylerdavis/.pub-cache/hosted/pub.dev/firebase_core-4.1.0/macos/firebase_core"),
         .package(name: "shared_preferences_foundation", path: "/Users/kylerdavis/.pub-cache/hosted/pub.dev/shared_preferences_foundation-2.5.4/darwin/shared_preferences_foundation")
     ],
     targets: [
